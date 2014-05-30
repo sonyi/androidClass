2b8b8e793a1f9e8640fb64dcddcc9d6bf89e3634
sonyi <jideyaoxingfu@gmail.com>
2014-05-30 22:44:27 +0800

    5.30666666666666666

--- day05/ListViewTest/src/com/example/listviewtest/MainActivity.java
+++ day05/ListViewTest/src/com/example/listviewtest/MainActivity.java
@@ -24,36 +24,47 @@ public class MainActivity extends Activity {
 	private ListView lvInfoList;
 	private List<Feed> mDataSource = new ArrayList<Feed>();
 	private FeedListAdapter mAdapter;
+	private TextView tv_footer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		lvInfoList = (ListView) findViewById(R.id.lv_info);
-		initData();
-		mAdapter = new FeedListAdapter();
+		mAdapter = new FeedListAdapter();//创建适配器
+		initData();//初始化数据
 		
-		View headerView = getLayoutInflater().inflate(R.layout.view_info_header, null);
+		//添加头部视图
+		View headerView = getLayoutInflater().inflate(
+				R.layout.view_info_header, null);
 		lvInfoList.addHeaderView(headerView);
+
 		
-		View footerView = getLayoutInflater().inflate(R.layout.view_info_foot, null);
+		//添加尾部视图并添加监听器
+		View footerView = getLayoutInflater().inflate(R.layout.view_info_foot,
+				null);
 		lvInfoList.addFooterView(footerView);
+		tv_footer = (TextView) footerView.findViewById(R.id.tv_footer);
+		tv_footer.setOnClickListener(mFooterViewClickListener);
 
-		
-				
+		//加载适配器，必须放在所以视图加载后再设置适配器
 		lvInfoList.setAdapter(mAdapter);
-		
-		lvInfoList.setOnItemClickListener(new OnItemClickListener() {
 
+		//添加视图单击监听事件
+		lvInfoList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
 				// TODO Auto-generated method stub
-				Feed feed = mDataSource.get(arg2);
-				Toast.makeText(MainActivity.this, feed.getTitle(),
-						Toast.LENGTH_SHORT).show();
+				arg2--;
+				if (arg2 >= 0 && arg2 < mDataSource.size()) {// 对点击的视图进行判断，排除头尾视图
+					Feed feed = mDataSource.get(arg2);
+					Toast.makeText(MainActivity.this, feed.getTitle(),
+							Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
+
 	}
 
 	public void initData() {
@@ -78,6 +89,7 @@ public class MainActivity extends Activity {
 
 	private class FeedListAdapter extends BaseAdapter {
 		int count = 0;
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
@@ -102,7 +114,7 @@ public class MainActivity extends Activity {
 			// convertView是重用的子项视图对象，如果不为空，就不比执行inflate和findViewById等消耗操作
 			View view = arg1;
 			ViewHolder vh = null;
-			
+
 			if (view == null) {
 				// convertView为空，inflate子项视图，加载控件实例
 				view = getLayoutInflater().inflate(R.layout.view_info_item,
@@ -115,20 +127,12 @@ public class MainActivity extends Activity {
 				vh.btnAction = (ImageButton) view.findViewById(R.id.btn_action);
 				// 将ViewHolder对象与子项视图对象关联
 				view.setTag(vh);
-				
-				Log.i("view", "创建了第" + (count++) + "个view");
-			} 
-			else {
+
+				Log.i("view", "创建了第" + (++count) + "个view");
+			} else {
 				vh = (ViewHolder) view.getTag();
 			}
 
-//			ImageView imgAvater = (ImageView) view.findViewById(R.id.img_avater);
-//			TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
-//			TextView tvDescription = (TextView) view
-//					.findViewById(R.id.tv_description);
-//			ImageButton btnAction = (ImageButton) view.findViewById(R.id.btn_action);
-			
-			
 			Feed data = mDataSource.get(arg0);
 			vh.imgAvater.setImageResource(data.getImgResId());
 			vh.tvTitle.setText(data.getTitle());
@@ -156,6 +160,23 @@ public class MainActivity extends Activity {
 		}
 
 	}
+
+	private OnClickListener mFooterViewClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Feed feed1 = new Feed(1, R.drawable.img_01, "这是标题1", "这是图片注释");
+			Feed feed2 = new Feed(2, R.drawable.img_02, "这是标题2", "这是图片注释");
+			Feed feed3 = new Feed(3, R.drawable.img_03, "这是标题3", "这是图片注释");
+			Feed feed4 = new Feed(4, R.drawable.img_04, "这是标题4", "这是图片注释");
+			mDataSource.add(feed1);
+			mDataSource.add(feed2);
+			mDataSource.add(feed3);
+			mDataSource.add(feed4);
+			mAdapter.notifyDataSetChanged();
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
