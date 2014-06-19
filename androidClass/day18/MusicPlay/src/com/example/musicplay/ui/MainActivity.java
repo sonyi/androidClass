4299f01cbc25e75272f55e5c66977fc1abf04cde
sonyi <jideyaoxingfu@gmail.com>
2014-06-19 08:18:42 +0800

    6.190

---
+++ androidClass/day18/MusicPlay/src/com/example/musicplay/ui/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.musicplay.ui;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+import com.example.musicplay.R;
+import com.example.musicplay.data.AlbumDataAccess;
+import com.example.musicplay.data.MusicDataAccess;
+import com.example.musicplay.model.Album;
+import com.example.musicplay.model.Music;
+
+public class MainActivity extends Activity {
+	private ArrayList<Album> mAlbumArray;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+//		mAlbumArray = (ArrayList<Album>) new AlbumDataAccess(this).getAllAlbumList();
+//		for(Album al : mAlbumArray){
+//			Log.i("music", al.toString());
+//		}
+		
+		ArrayList<Music> musicArray = (ArrayList<Music>) new MusicDataAccess(this).getAllMusic();
+		for(Music m : musicArray){
+			Log.i("music", m.toString());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
